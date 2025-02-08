ddc1f2cc3451fe12382c3a112584944d1c8da899
Sten Johnsen <sten@thinea.no>
2025-02-08 08:44:04 +0100

    prvate constructor

--- src/models/FileName.test.ts
+++ src/models/FileName.test.ts
@@ -4,24 +4,32 @@ import { FileName } from './FileName';
 describe('FileName', () => {
   it('should create a FileName instance with the given value', () => {
     const value = 'example.txt';
-    const fileName = new FileName(value);
+    const fileName = FileName.create(value);
 
     expect(fileName.toString()).toEqual(value);
   });
 
-  it('should throw an error if the filename exceeds the maximum length', () => {
-    const value = 'a'.repeat(61);
+      it('should throw an error if the filename exceeds the maximum length', () => {
+      const value = 'a'.repeat(61);
 
-    expect(() => new FileName(value)).toThrowError(
-      `Filename exceeds max length of ${FileName.MAX_LENGTH} characters.`
-    );
-  });
+      expect(() => FileName.create(value)).toThrowError(
+        `Filename exceeds max length of ${FileName.MAX_LENGTH} characters.`
+      );
+    });
 
-  it('should throw an error if the filename contains invalid characters', () => {
-    const value = 'example@file.txt';
+    it('should throw an error if the filename contains invalid characters', () => {
+      const value = 'example@file.txt';
 
-    expect(() => new FileName(value)).toThrowError(
-      'Filename contains invalid characters. Allowed: letters, numbers, dots, dashes, and underscores.'
-    );
+      expect(() => FileName.create(value)).toThrowError(
+        'Filename contains invalid characters. Allowed: letters, numbers, dots, dashes, and underscores.'
+      );
+    });
+
+  it('should throw an error if the filename is a number', () => {
+    const value = 123;
+
+    expect(() => FileName.create(value)).toThrowError(
+      'Filename is not a string.'
+    );    
   });
 });
--- src/models/FileName.ts
+++ src/models/FileName.ts
@@ -1,23 +1,25 @@
 import { DomainModel } from "./DomainModel";
 
 export class FileName extends DomainModel<string> {
-    private static readonly MAX_LENGTH = 60;
+    static readonly MAX_LENGTH = 60;
     private static readonly ALLOWED_CHARACTERS = /^[a-zA-Z0-9._-]+$/;
-    
-    constructor(filename: string) {
-        super(filename);
-    }
     
     static create(filename: any): FileName {
         return new FileName(filename);
     }
     /** Ensure filename follows rules */
     protected validate(): void {
+
+        if (typeof this.value !== "string") {
+            throw new Error("Filename is not a string.");
+        }
         if (this.value.length > FileName.MAX_LENGTH) {
             throw new Error(`Filename exceeds max length of ${FileName.MAX_LENGTH} characters.`);
-        }
+         }
+
         if (!FileName.ALLOWED_CHARACTERS.test(this.value)) {
             throw new Error("Filename contains invalid characters. Allowed: letters, numbers, dots, dashes, and underscores.");
         }
     }
+
 }
